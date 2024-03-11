f2c75c3abc35724fa84445698864f49238645aa5
UrielMorales <uriel@gmail.com>
2024-03-11 09:45:56 -0600

    se agrego tamaño movil

--- header.php
+++ header.php
@@ -1,72 +1,133 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="https://kit.fontawesome.com/cedfbf939d.js" crossorigin="anonymous"></script>
-    <!-- <link rel="icon" type="image/PNG" href="img/logo1.png"> -->
-    <link rel="stylesheet" href="css/font.css">
-    <link rel="stylesheet" href="css/menu_oculto.css">
-    <script src="https://kit.fontawesome.com/cedfbf939d.js" crossorigin="anonymous"></script>
-</head>
-<body>
 
 
-<header class="header_Contec_menu_pequeño">
-    <!-- <div class="header__superior">
-        
-        
-        
-    </div> -->
-    <div class="header__superior">
-            <div class="logo">
-                <img class="loguito" src="img/logo_home_ssm.svg" alt="">
+ <!--=============== REMIXICONS ===============-->
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/css/styles.css">
+   <!--=============== HEADER ===============-->
+   <header class="header">
+         <nav class="nav container">
+            <div class="nav__data">
+               <a href="index.php" class="nav__logo">
+               <!-- <i class="ri-planet-line"></i> Company -->
+                  <img src="img/logo_home_ssm.svg" class="logo_pequeño" alt="">
+               </a>
+            <div class="nav__toggle" id="nav-toggle">
+               <i class="ri-menu-line nav__burger"></i>
+               <i class="ri-close-line nav__close"></i>
             </div>
-            <div class="search">
-            <!-- <div class="infoTelMail">
-            <a href="facebook.com" class="iconInfo"><h1 class="infophone"><i class="fa-solid fa-phone"></i> 9513094613</h1></a>
-            <a href="#" class="iconInfo"><h1 class="infoMail"><i class="fa-regular fa-envelope"></i> admin@rf8income.com.mx</h1></a>
-        </div> -->
-            <div class="infoIconi">
-            
-            <a href="#" class="iconInfo2"><i class="fa-brands fa-facebook-f iconos_header"></i></a>
-            <!-- <a href="https://www.youtube.com/watch?v=9CiK8yEaKqY" class="iconInfo2"><i class="fa-brands fa-instagram iconos_header"></i></a> -->
-            <!-- <a href="https://www.youtube.com/watch?v=9CiK8yEaKqY" class="iconInfo2"><i class="fa-brands fa-twitter iconos_header"></i></a> -->
-            <!-- <a href="https://www.youtube.com/watch?v=9CiK8yEaKqY" class="iconInfo2"><i class="fa-brands fa-linkedin iconos_header"></i></a> -->
-        </div>
             </div>
-        </div>
+
+            <!--=============== NAV MENU ===============-->
+            <div class="nav__menu" id="nav-menu">
+               <ul class="nav__list">
+                  <li><a href="index.php" class="nav__link">HOME</a></li>
+
+                  <!-- <li><a href="#" class="nav__link">¿QUÉ ES?</a></li> -->
+
+                  <!--=============== DROPDOWN 1 ===============-->
+                  <!-- <li class="dropdown__item">
+                     <div class="nav__link">
+                        Analytics <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                     </div>
+
+                     <ul class="dropdown__menu">
+                        <li>
+                           <a href="#" class="dropdown__link">
+                              <i class="ri-pie-chart-line"></i> Overview
+                           </a>                          
+                        </li>
+
+                        <li>
+                           <a href="#" class="dropdown__link">
+                              <i class="ri-arrow-up-down-line"></i> Transactions
+                           </a>
+                        </li> -->
+
+                        <!--=============== DROPDOWN SUBMENU ===============-->
+                        <!-- <li class="dropdown__subitem">
+                           <div class="dropdown__link">
+                              <i class="ri-bar-chart-line"></i> Reports <i class="ri-add-line dropdown__add"></i>
+                           </div>
+
+                           <ul class="dropdown__submenu">
+                              <li>
+                                 <a href="#" class="dropdown__sublink">
+                                    <i class="ri-file-list-line"></i> Documents
+                                 </a>
+                              </li>
+      
+                              <li>
+                                 <a href="#" class="dropdown__sublink">
+                                    <i class="ri-cash-line"></i> Payments
+                                 </a>
+                              </li>
+      
+                              <li>
+                                 <a href="#" class="dropdown__sublink">
+                                    <i class="ri-refund-2-line"></i> Refunds
+                                 </a>
+                              </li>
+                           </ul>
+                        </li>
+                     </ul>
+                  </li> -->
+                  
+                  <li><a href="catalogo.php" class="nav__link">CATÁLOGO</a></li>
+
+                  <!--=============== DROPDOWN 2 ===============-->
+                  <!-- <li class="dropdown__item">
+                     <div class="nav__link">
+                        Users <i class="ri-arrow-down-s-line dropdown__arrow"></i>
+                     </div>
+
+                     <ul class="dropdown__menu">
+                        <li>
+                           <a href="#" class="dropdown__link">
+                              <i class="ri-user-line"></i> Profiles
+                           </a>                          
+                        </li>
+
+                        <li>
+                           <a href="#" class="dropdown__link">
+                              <i class="ri-lock-line"></i> Accounts
+                           </a>
+                        </li>
+
+                        <li>
+                           <a href="#" class="dropdown__link">
+                              <i class="ri-message-3-line"></i> Messages
+                           </a>
+                        </li>
+                     </ul>
+                  </li> -->
+
+                  <li><a href="ventajas.php" class="nav__link">VENTAJAS</a></li>
+
+                  <li><a href="proyectos.php" class="nav__link">PROYECTOS</a></li>
+                  
+                  <li><a href="contacto.php" class="nav__link">CONTACTO</a></li>
+
+                  <li><a href="fqa.php" class="nav__link">FAQ</a></li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+      <!--=============== MAIN JS ===============-->
+      <script src="assets/js/main.js"></script>
+
+      <style>
+        .logo_pequeño{
+            width: 20%;
+        }
 
 
-    <div class="container__menu">
-        <div class="menu__peeque">
-        <input type="checkbox" id="check__menu">
-                <label for="check__menu" id="label__check">
-                    <i class="fas fa-bars icon__menu"></i>
-                </label>
-            <nav class="nav_menupeque">
-                <ul class="ul_menupeque">
-                    <li class="li__menupeque"><a class="a__menupeque" href="index.php">INICIO</a></li>
-                    <li class="li__menupeque"><a class="a__menupeque" href="#">NOSOTROS</a>
-                        <ul class="ul_menupeque">
-                            <li class="li__menupeque"> <a class="a__menupeque" href="about.php">ACERCA DE INCOME</a></li>
-                            <li class="li__menupeque" ><a class="a__menupeque" href="equipment.php">EQUIPÓS E INSTALACIONES</a></li>
-                            <li class="li__menupeque" ><a class="a__menupeque" href="security.php">SEGURIDAD</a></li>
-                            <li class="li__menupeque" ><a class="a__menupeque" href="ethics.php">ÉTICA</a></li>
-                        
-                        </ul>
-                    </li>
-                    <li  class="li__menupeque"><a class="a__menupeque" href="#">SERVICIOS</a>
-                        <ul class="ul_menupeque">
-                            <li class="li__menupeque"><a class="a__menupeque" href="services.php">NUESTROS SERVICIOS</a></li>
-                            <li class="li__menupeque"><a class="a__menupeque" href="intranet.php">INTRANET</a></li>                   
-                        </ul>
-                    </li>
-                    <li class="li__menupeque"><a class="a__menupeque" href="contact.php">CONTACTO</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+        @media screen and (min-width: 1120px)
+         
+        {
+            header{
+                display: none;
+            } 
+         }
 
-
-</body>
+      </style>
